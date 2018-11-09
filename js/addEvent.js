3722f3eb9a82fb853f36ef0ce798d12d06b06b44
goldhardtr Laptop <blackfisch@outlook.com>
2018-11-09 22:39:26 +0100

    Read and Write to DB

--- js/addEvent.js
+++ js/addEvent.js
@@ -23,31 +23,52 @@ let indexLookup = {
     "Committed": 4
 };
 
-async function loadHome() {
-    let data = [];
-    let events = await db.collection("events").get().then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            if (doc.exists) {
-                console.log(true);
-                data[0]=(doc.data());
-            }
-        });
-    });
-    console.log(data.length);
+async function addEvent(form) {
+    let topic = form.topic.value;
+    let description = form.description.value;
+    let maxcommit = form.maxcommit.value;
+    let location = form.location.value;
+    let adress = form.adress.value + ", " + location;
+    let date = form.date.value;
+    let time = form.time.value;
+    let timestamp = Date.parse(date.toString()+ ' ' + time.toString());
 
-    let table = document.getElementById('event-list');
-    let entries = table.rows.length;
-
-    data.forEach ((el, i) => {
-        let row = table.insertRow(entries+i);
-
-        row.insertCell(indexLookup.Topic).innerHTML = el.Topic;
-        row.insertCell(indexLookup.Location).innerHTML = el.Location;
-        row.insertCell(indexLookup.Date).innerHTML = unixToDate(el.Date.seconds);
-        row.insertCell(indexLookup.MaxCommit).innerHTML = el.MaxCommit;
-        row.insertCell(indexLookup.Committed).innerHTML = el.Committed;
+    await db.collection("events").doc().set({
+        Topic: topic,
+        Location: location,
+        Date: timestamp,
+        MaxCommit: maxcommit,
+        Committed: 0,
+        Description: description,
+        Adress : adress
     });
 }
+
+// async function loadHome() {
+//     let data = [];
+//     let events = await db.collection("events").get().then((querySnapshot) => {
+//         querySnapshot.forEach((doc) => {
+//             if (doc.exists) {
+//                 console.log(true);
+//                 data[0]=(doc.data());
+//             }
+//         });
+//     });
+//     console.log(data.length);
+//
+//     let table = document.getElementById('event-list');
+//     let entries = table.rows.length;
+//
+//     data.forEach ((el, i) => {
+//         let row = table.insertRow(entries+i);
+//
+//         row.insertCell(indexLookup.Topic).innerHTML = el.Topic;
+//         row.insertCell(indexLookup.Location).innerHTML = el.Location;
+//         row.insertCell(indexLookup.Date).innerHTML = unixToDate(el.Date.seconds);
+//         row.insertCell(indexLookup.MaxCommit).innerHTML = el.MaxCommit;
+//         row.insertCell(indexLookup.Committed).innerHTML = el.Committed;
+//     });
+// }
 
 //Source: https://makitweb.com/convert-unix-timestamp-to-date-time-with-javascript/
 function unixToDate(timestamp) {
