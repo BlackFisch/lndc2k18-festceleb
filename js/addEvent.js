3dd30c25d88c8833f3c5d1140087fc81dfc369f9
goldhardtr Laptop <blackfisch@outlook.com>
2018-11-09 22:48:18 +0100

    format time correctly

--- js/addEvent.js
+++ js/addEvent.js
@@ -15,14 +15,6 @@ db.settings({
   timestampsInSnapshots: true
 });
 
-let indexLookup = {
-    "Topic": 0,
-    "Location": 1,
-    "Date": 2,
-    "MaxCommit": 3,
-    "Committed": 4
-};
-
 async function addEvent(form) {
     let topic = form.topic.value;
     let description = form.description.value;
@@ -31,7 +23,7 @@ async function addEvent(form) {
     let adress = form.adress.value + ", " + location;
     let date = form.date.value;
     let time = form.time.value;
-    let timestamp = Date.parse(date.toString()+ ' ' + time.toString());
+    let timestamp = new Date(Date.parse(date.toString()+ ' ' + time.toString()));
 
     await db.collection("events").doc().set({
         Topic: topic,
@@ -42,62 +34,4 @@ async function addEvent(form) {
         Description: description,
         Adress : adress
     });
-}
-
-// async function loadHome() {
-//     let data = [];
-//     let events = await db.collection("events").get().then((querySnapshot) => {
-//         querySnapshot.forEach((doc) => {
-//             if (doc.exists) {
-//                 console.log(true);
-//                 data[0]=(doc.data());
-//             }
-//         });
-//     });
-//     console.log(data.length);
-//
-//     let table = document.getElementById('event-list');
-//     let entries = table.rows.length;
-//
-//     data.forEach ((el, i) => {
-//         let row = table.insertRow(entries+i);
-//
-//         row.insertCell(indexLookup.Topic).innerHTML = el.Topic;
-//         row.insertCell(indexLookup.Location).innerHTML = el.Location;
-//         row.insertCell(indexLookup.Date).innerHTML = unixToDate(el.Date.seconds);
-//         row.insertCell(indexLookup.MaxCommit).innerHTML = el.MaxCommit;
-//         row.insertCell(indexLookup.Committed).innerHTML = el.Committed;
-//     });
-// }
-
-//Source: https://makitweb.com/convert-unix-timestamp-to-date-time-with-javascript/
-function unixToDate(timestamp) {
-	// Convert timestamp to milliseconds
-	let date = new Date(timestamp*1000);
-
-	// retrieve date
-	let year = date.getFullYear();
-	let month = 1+date.getMonth();
-	let day = date.getDate();
-
-	// retrieve time
-	let hours = date.getHours();
-	let minutes = "0" + date.getMinutes();
-	let seconds = "0" + date.getSeconds();
-
-	// Display date in dd.MM.yyyy format
-	return (day + '.' + month + '.' + year);
-}
-function unixToDateTime(timestamp) {
-    let dateFormat = unixToDate(timestamp);
-	// Convert timestamp to milliseconds
-	let date = new Date(timestamp*1000);
-
-	// retrieve time
-	let hours = date.getHours();
-	let minutes = "0" + date.getMinutes();
-	let seconds = "0" + date.getSeconds();
-
-	// Display date time in dd.MM.yyyy h:m:s format
-	return (dateFormat + ' ' + hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2));
 }
