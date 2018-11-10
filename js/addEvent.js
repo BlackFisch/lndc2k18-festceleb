d5109336710221f877e770e7ce6ae0f34255e0b9
goldhardtr Laptop <blackfisch@outlook.com>
2018-11-10 08:00:00 +0100

    checks

--- js/addEvent.js
+++ js/addEvent.js
@@ -1,19 +1,35 @@
+function validateInputs(topic, desc, max, loc, adr, time) {
+    if (topic === "" || desc === "" || max <= 0 || loc === "") return false;
+    if (Date.now() >= time) return false;
+    // if (time.getFullYear() > (Date.now().getFullYear() + 1)) return false;
+    if (topic.length > 100 || desc.length > 500 || max > 50 || loc.length > 200) return false;
+
+
+    return true;
+}
+
 async function addEvent(form) {
     let topic = form.topic.value;
-    form.topic.value = "";
     let description = form.description.value;
-    form.description.value = "";
     let maxcommit = form.maxcommit.value;
-    form.maxcommit.value = "";
     let location = form.location.value;
-    form.location.value = "";
     let adress = form.adress.value + ", " + location;
-    form.adress.value = "";
     let date = form.date.value;
-    form.date.value = "";
     let time = form.time.value;
-    form.time.value = "";
     let timestamp = new Date(Date.parse(date.toString()+ ' ' + time.toString()));
+
+    if (!validateInputs(topic,description,maxcommit,location,adress,timestamp)) {
+        alert("Bitte überprüfen Sie Ihre Eingaben!");
+        return;
+    }
+
+    form.topic.value = "";
+    form.description.value = "";
+    form.maxcommit.value = "";
+    form.location.value = "";
+    form.adress.value = "";
+    form.date.value = "";
+    form.time.value = "";
 
     await db.collection("events").doc().set({
         Topic: topic,
